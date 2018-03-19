6fc4050c1d4d2080a7ab2f3fc33c7ca37ce4f8d0
Reinoldo sommer <reinoldo@outlook.com>
2018-03-19 15:51:05 -0300

    Melhoria na solução 4

--- src/com/reinoldo/problems/Poblema04.java
+++ src/com/reinoldo/problems/Poblema04.java
@@ -1,31 +1,47 @@
 package com.reinoldo.problems;
 
-
+import java.util.Scanner;
 
 public class Poblema04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		long maiorPalindromo = 11;
-		long produto = 0;
-		for(long i = 999;i >=900; i--){
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("Digite a quantidade de digitos a ser multiplicado (Ex: 3): ");
+		int digitos = in.nextInt();
+		
+		//inicializa o array 
+		char[] palavra = new char[digitos];		
+		
+		for(int i = 0; i < digitos;i++){
+			palavra[i] = '9';
 			
-			for(long j = 999; j>=900;j--){
+		}
+		//Transforma o array de char em String
+		String palavra2 = new String(palavra);
+		Integer numeros = 0;
+		//Converte String em inteiro
+		numeros = numeros.parseInt(palavra2);
+		
+		
+		long maiorPalindromo = 2;
+		long produto = 0;
+		
+		for(long i = numeros;i >=1; i--){
+			
+			for(long j = numeros; j>=1;j--){
 				produto = i*j;
 				if(maiorPalindromo < produto && ePalindromo(String.valueOf(produto))){
 					maiorPalindromo = produto;
+					break;
 				}
 				
 			}
 		}
 		System.out.println(maiorPalindromo);
-		  //System.out.println("1. " + ePalindromo("9009"));
+		  
 		
 
-	}
-	
-	public static boolean ePalindromo2(String palavra) {
-	    return palavra.equals(new StringBuilder(palavra).reverse().toString());
 	}
 	
 	 public static boolean ePalindromo(String palavra) {
